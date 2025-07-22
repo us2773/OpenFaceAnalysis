69dbbf390becadd449018b28a517ae97e65a2618
us2773 <breeze.prunus0707@gmail.com>
2025-07-22 16:21:23 +0900

    クラス・コンストラクタ定義

---
+++ modules/analysis.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class Analyzer :
+    def __init__(self, csv_path) :
+        self.path = csv_path
+        self.df = self._load_csv()
+        
+    def _load_csv(self) -> pd.DataFrame:
+        try:
+            df = pd.read_csv(self.csv_path)
+            return df
+        except Exception as e:
+            raise RuntimeError(f"fail to load csv: {e}")
