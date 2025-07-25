f88f4288b3db6d77de903194a11b0942c550bb40
us2773 <breeze.prunus0707@gmail.com>
2025-07-25 19:13:18 +0900

    AU_noise_extraction整理

--- modules/AU_noise_extraction.py
+++ modules/AU_noise_extraction.py
@@ -5,31 +5,31 @@ import os
 import glob
 from statsmodels.nonparametric.smoothers_lowess import lowess
 
-
-
 file_directory = "250716"
-#file_name = "VID20250701164140"
 
+# 結果出力ディレクトリのチェック・新規作成
 files = glob.glob(f"./output/{file_directory}/VID*.csv")
-#print(files)
 files = [os.path.splitext(os.path.basename(x))[0] for x in files]
-#print(files)
 
-
+# AUをノイズとトレンドに分離する関数
 def get_AU_noise(file_name) :
     print(file_name)
     df = pd.read_csv(f"./output/{file_directory}/{file_name}.csv")
 
+    # 無関係なデータ行削除
     df = df[(df[" success"] == 0) | (df[" success"] == 1)]
 
+    # 成功率チェック
     num_of_row = df.shape[0]
     success = np.array(df[" success"])
     count_success = success.sum()
     print(count_success, num_of_row)
     print(f"success rate: {(count_success/num_of_row*100):.2f}%")
 
+    # AU種類指定
     au_col = " AU01_r"
     
+    # LOWESS局所回帰によるトレンド抽出
     trend_est = lowess(df[au_col], df[" timestamp"], frac=0.1, return_sorted=False)
     
     # 残差（不規則変動成分）
@@ -37,6 +37,7 @@ def get_AU_noise(file_name) :
     df[f"{au_col}_trend"] = trend_est
     df[f"{au_col}_fluct"] = residual
     
+    # 基本統計値の算出
     mean = df[f"{au_col}_fluct"].mean()
     print(f"mean: {mean}")
     var = df[f"{au_col}_fluct"].var()
@@ -44,24 +45,17 @@ def get_AU_noise(file_name) :
     std = df[f"{au_col}_fluct"].std()
     print(f"cv: {std / mean}")
     
-    # 可視化
+    # プロット化・保存
     plt.figure(figsize=(10, 4))
     plt.plot(df[" timestamp"], df[f"{au_col}"], label="Original")
-    #plt.show()
     plt.plot(df[" timestamp"], df[f"{au_col}_trend"], label="Trend (LOWESS)", color="green")
-    #plt.show()
     plt.plot(df[" timestamp"], df[f"{au_col}_fluct"], label="Residual (Fluctuation)", color="red", linestyle="--")
     plt.legend()
     plt.xlabel("Time [s]")
     plt.ylabel(f"{au_col}")
     plt.title(f"{au_col}: Trend and Fluctuation")
     plt.tight_layout()
-    #plt.show()
     plt.savefig(f"./result/{file_directory}/{file_name}_{au_col}_trend_noise.png", bbox_inches="tight")
     
-
 for i in range(len(files)):
     get_AU_noise(files[i])
-
-
-#get_AU_noise("VID_20250707163518_bangsup_f2")
